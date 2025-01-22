790ab88a76a39135bcc15fed1efb0316ee7ebcf1
GreyA <grey.altaer@gmail.com>
2025-01-22 12:26:34 -0800

    exercise 8 completed

--- app.js
+++ app.js
@@ -138,3 +138,29 @@ const calculateTip = (billAmount, tipPercent) => {
 };
 
 console.log("Exercise 7 Result:", calculateTip(50, 20));
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature = (degrees, scale) => {
+  if (scale === "C") {
+    const cenToF = () => (degrees * 9) / 5 + 32;
+    return `${cenToF()}` + "(Fahrenheit)";
+  } else if (scale === "F") {
+    const fahrToC = () => ((degrees - 32) * 5) / 9;
+    return `${fahrToC}` + "(Celsius)";
+  }
+};
+
+console.log("Exercise 8 Result:", convertTemperature(32, "C"));
